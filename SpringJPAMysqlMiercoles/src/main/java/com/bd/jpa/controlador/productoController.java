3ebca125f3a44884c069aaf3b9e9193b98ab6672
hawell <hawell@gmail.com>
2025-06-18 12:36:39 -0500

    Agregar el Registro de productos

--- SpringJPAMysqlMiercoles/src/main/java/com/bd/jpa/controlador/productoController.java
+++ SpringJPAMysqlMiercoles/src/main/java/com/bd/jpa/controlador/productoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bd.jpa.modelo.TblProducto;
@@ -16,15 +18,27 @@ public class productoController {
 	@Autowired
 	private IProductoService productoServ;
 	
-	@GetMapping("/ListadoProducto")
+	@GetMapping("/ListadoProductos")
 	public String ListarProducto(Model model) {
 		model.addAttribute("producto",productoServ.ListadoProducto());
 		
 		return "/vistas/ListadoProducto";
 	}
 	@GetMapping("/RegistrarProducto")
-	public String RegistrarProducto(TblProducto producto ,Model model) {
+	public String RegistrarProducto(Model model) {
+		TblProducto tblprod=new TblProducto();
+		
+		model.addAttribute("regproducto",tblprod);
 		
 		return "/vistas/RegistrarProducto";
+	}
+	@PostMapping("/GuardarProducto")
+	public String GuardarProducto(@ModelAttribute TblProducto tblprod,Model modelo) {
+		productoServ.RegistrarProducto(tblprod);
+		System.out.println("Dato Registrado en la BD !!!!!");
+		
+		return "redirect:/vistas/ListadoProductos";
+		
+		
 	}
 }
